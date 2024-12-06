069eb0a32f581927c3b13e885145bfa15abd4cc6
fattahali37 <fattahali37@gmail.com>
2024-12-06 18:21:02 +0500

    contact updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-import Prescripto from "../assets/projects/Prescripto.png"
+import Prescripto from "../assets/projects/Prescripto.png";
 import project2 from "../assets/projects/project-2.jpeg";
 import project3 from "../assets/projects/project-3.jpeg";
 import project4 from "../assets/projects/project-4.jpeg";
@@ -70,7 +70,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Lahore, Pakistan ",
+  phoneNo: "+92 300 4595764 ",
+  email: "fattahali37@gmail.com",
 };
